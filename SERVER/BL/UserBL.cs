5f5e3e73279d01ece9e4c7a3084453b06c47a571
GalAsraf <asraf.gal012@gmail.com>
2020-09-10 23:05:04 +0300

    try server commit

---
+++ SERVER/BL/UserBL.cs
@@ -0,0 +1,22 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class UserBL
+    {
+        public static void AddUser(UserDTO user)
+        {
+            using(RecipezeEntities db = new RecipezeEntities())
+            {
+                db.Users.Add(CONVERTERS.UserConverter.ConvertUserToDAL(user));
+                db.SaveChanges();
+            }
+        }
+    }
+}
