2a367b7c389c728f64a38fbf6f697298d9eb2a1e
GalAsraf <asraf.gal012@gmail.com>
2020-09-17 18:16:34 +0300

    client try

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AddUserComponent } from './components/add-user/add-user.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddUserComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
