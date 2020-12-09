2671dac525b8906dd951b4af4fadb9b803035356
GalAsraf <asraf.gal012@gmail.com>
2020-12-09 17:35:17 +0200

    fixed cookbook

--- app/src/app/components/cookbook/cookbook.component.ts
+++ app/src/app/components/cookbook/cookbook.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { Recipe } from 'src/app/shared/models/recipe.model';
 import { RecipeService } from 'src/app/shared/services/recipe.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DialogService } from 'primeng/dynamicdialog';
+import { CurrentRecipeComponent } from '../current-recipe/current-recipe.component';
+
+
 
 @Component({
   selector: 'app-cookbook',
@@ -10,32 +14,59 @@ import { RecipeService } from 'src/app/shared/services/recipe.service';
 })
 export class CookbookComponent implements OnInit {
   cookbookList: Recipe[] = [];
+  cookbookToShow: Recipe[] = [];
   title = "angular-text-search-hightlight";
-  searchText='';
-  characters=[
+  searchText = '';
+  characters = [
     'chocolate chip cookies',
     'tuna pasta salad',
     'sweet potato fries',
     'avocado salad',
     'vanilla cake',
     'chocolate mousse'
-  ]
-  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
+  ];
+  // cookbookToShow: string[] = [];
+
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService,
+     private router: Router) { }
+// public dialogService: DialogService
 
   ngOnInit(): void {
     this.recipeService.getUserCookbook().subscribe(
       res => {
         this.cookbookList = res;
         console.log(res)
+        this.showRecipeList();
       },
       err => { console.error(err) }
     );
+
+
+  }
+
+  showRecipeList() {
+    this.cookbookToShow = this.cookbookList;
+    // this.cookbookList.forEach(r => {
+    //   this.cookbookToShow.push(r.RecipeName)
+    // });
   }
 
 
-  removeRecipeFromCookbook(recipe:Recipe){
+  removeRecipeFromCookbook(recipe: Recipe) {
     this.recipeService.deleteRecipeFromCookbook(recipe).subscribe(
       res => console.log(res));
+  }
+
+
+  showRecipe(recipe:Recipe){
+    this.router.navigate(['current-recipe',JSON.stringify(recipe)]);
+
+    
+  //   const ref = this.dialogService.open(CurrentRecipeComponent, {
+  //     data: {currentRecipe:recipe},
+  //     header: recipe.RecipeName,
+  //     width: '70%'
+  // });
   }
 
 
